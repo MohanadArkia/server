6d755439abd7a01d0df42e632c1ba0557735b59a
MohanadArkia <arkia.ar.47@gmail.com>
2023-11-11 14:32:54 +0200

    Display HTML instead of regular text

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const http = require("http");
 
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./index.html");
+const aboutPage = readFileSync("./pages/about.html");
+const errorPage = readFileSync("./pages/error-page.html");
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-type": "text/plain" });
-  response.end("Welcome");
+  const url = request.url;
+
+  if (url === "/") {
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.write(homePage);
+    response.end();
+  } else if (url === "/about") {
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.write(aboutPage);
+    response.end();
+  } else {
+    response.writeHead(404, { "Content-type": "text/html" });
+    response.write(errorPage);
+    response.end();
+  }
 });
 
 server.listen(8080, () => {
